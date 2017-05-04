d033b86b3f49defafb7ebc2a8d8892c96c5ce348
Unknown <clementserrano3@gmail.com>
2017-05-04 08:33:56 +0200

    Client début

---
+++ src/Client.java
@@ -0,0 +1,16 @@
+import java.net.InetAddress;
+
+public class Client {
+
+    public static void main(String[] args) {
+        // do things
+    }
+
+    public void receiveFile(InetAddress adresse, int port, String fichierDistant, String fichierLocal){
+
+    }
+
+    public void sendFile(InetAddress adresse, int port, String fichierLocal){
+
+    }
+}
